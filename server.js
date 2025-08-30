f945ef97535edbb954525ae8ea6f43642e415ea1
josetoroc <47046906+josetoroc@users.noreply.github.com>
2025-08-30 13:35:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hola Mundo!');
 });
 
 server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
+
 });
